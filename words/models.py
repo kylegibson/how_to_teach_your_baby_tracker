6d1f13361b59a78d9c615cc449a4fbf12ed2351f
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-01-01 15:49:47 -0500

    Default to 0 views

--- words/models.py
+++ words/models.py
@@ -5,7 +5,7 @@ class Word(models.Model):
     word = models.CharField(max_length=255)
     date_retired = models.DateTimeField(null=True, blank=True)
     date_active = models.DateTimeField(null=True, blank=True)
-    views = models.IntegerField()
+    views = models.IntegerField(default=0)
 
     @property
     def is_active(self):
