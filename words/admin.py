f05c3b3bff7b2119ee0bd3bf16c8a58bb8ca3109
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-01-01 15:52:15 -0500

    Show all the fields

--- words/admin.py
+++ words/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from words.models import Word
 
 
-admin.site.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = [
+        'word',
+        'views',
+        'date_active',
+        'date_retired',
+    ]
+
+
+admin.site.register(Word, WordAdmin)
