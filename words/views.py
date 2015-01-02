8379e9b43ca851b4f6f4df22d01efbc1dc5e1385
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-01-01 23:03:28 -0500

    Atomically update the number of views

--- words/views.py
+++ words/views.py
@@ -1,12 +1,29 @@
+from django.db.models import F
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+
+from words.models import Word
 
 
 @require_POST
 def mark_word_viewed(request, word_id):
+    Word.objects.filter(
+        pk=word_id,
+        date_retired__isnull=True,
+        date_active__isnull=False,
+    ).update(
+        views=F('views') + 1,
+    )
     return HttpResponse('ok')
 
 
 @require_POST
 def mark_group_viewed(request, word_group_id):
+    Word.objects.filter(
+        grouping_id=word_group_id,
+        date_retired__isnull=True,
+        date_active__isnull=False,
+    ).update(
+        views=F('views') + 1,
+    )
     return HttpResponse('ok')
