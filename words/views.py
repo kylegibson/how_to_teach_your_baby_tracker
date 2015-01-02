6dd381f914010b13f11847647ae6f77c58ec1eaa
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-01-01 22:35:52 -0500

    Added missing request variable, renamed variables to match url parameters

--- words/views.py
+++ words/views.py
@@ -3,10 +3,10 @@ from django.views.decorators.http import require_POST
 
 
 @require_POST
-def mark_word_viewed(word):
+def mark_word_viewed(request, word_id):
     return HttpResponse('ok')
 
 
 @require_POST
-def mark_group_viewed(word):
+def mark_group_viewed(request, word_group_id):
     return HttpResponse('ok')
